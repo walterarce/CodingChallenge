2a0491badaf9882996abceea869387bdadf1d9c5
Walter Arce <walterarce@gmail.com>
2020-10-16 23:21:05 -0300

    Agrego implementacion del metodo calcularArea del Trapecio

--- CodingChallenge.Data/Classes/Trapecio.cs
+++ CodingChallenge.Data/Classes/Trapecio.cs
@@ -2,13 +2,23 @@
 {
     public class Trapecio : FormaGeometrica
     {
+        public decimal baseinferior { get; set; }
+        public decimal basesuperior { get; set; }
+        public decimal altura { get; set; }
         public Trapecio(decimal lado) : base(lado)
         {
+
         }
 
+        public Trapecio(decimal _basesuperior, decimal _baseinferior, decimal _altura)
+        {
+            altura = _altura;
+            baseinferior = _baseinferior;
+            basesuperior = _basesuperior;
+        }
         public override decimal CalcularArea()
         {
-            throw new System.NotImplementedException();
+            return (basesuperior + baseinferior) / 2 * altura;
         }
 
         public override decimal CalcularPerimetro()
